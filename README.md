832aae47cb730efbdc31cb4406710a1805f42efc
bahaamq <bahaa.mq@gmail.com>
2021-08-09 16:05:08 +0300

    readytodeploy

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# resty
+# SOLUTION - Component Based UI
+ UML Diagram (phase 1)
+ https://miro.com/app/board/o9J_l3PBzM0=/
---
+++ src/components/footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './footer.scss';
+
+
+//function App() {
+function Footer()
+{
+  return <footer>&copy; 2021</footer>;
+
+}
+
+
+export default Footer;
